df3d6fc1e4840d446729f1ed0e66ddb61ce42ebe
Taha Talat <tahatalattaha95@gmail.com>
2023-05-13 09:06:40 +0300

    add .py file

---
+++ time_delayed_print/time_delayed_print.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import time
+welcome_msg =" HELLO \n welcome to python tricks repo   \n\n\n here you could found some coll python codes\n\n"
+for char in welcome_msg:
+    print(char, end="", flush=True)  # Print each character without a newline and flush the buffer
+    time.sleep(0.04)
+            
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
